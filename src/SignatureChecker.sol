25d9b8616a7b8a6a4878dbbb2412703e5b4178d5
Waylon Jepsen <waylonjepsen1@gmail.com>
2025-02-10 14:11:46 -0700

    install openZ

--- src/SignatureChecker.sol
+++ src/SignatureChecker.sol
@@ -1,15 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import {ECDSA} from "./ECDSA.sol";
 
 // todo: Do we need to have some stateful capabilities?
 contract SignatureChecker {
     // todo Replace with our notary address
-    address public constant NOTARY_ADDRESS = address(0x34567890abcdef1234567890abcdef1234);
+    // 0xfdf07A5dCfa7b74f4c28DAb23eaD8B1c43Be801F
+    address public constant NOTARY_ADDRESS = address(0xfdf07A5dCfa7b74f4c28DAb23eaD8B1c43Be801F);
 
     function isValidSignatureNow(bytes32 hash, bytes memory signature) external pure returns (bool) {
         (address recovered, ECDSA.RecoverError err,) = ECDSA.tryRecover(hash, signature);
         return err == ECDSA.RecoverError.NoError && recovered == NOTARY_ADDRESS;
+
+        /// this decomposes the signature into r, s, v
+        // r is the x-coordinate of the curve point
+        // s is the y-coordinate of the curve point
+
+        // v is 0 for an even y-value
+        // v is 1 for an odd y-value
     }
 }
--- test/SignatureChecker.t.sol
+++ test/SignatureChecker.t.sol
@@ -16,6 +16,9 @@ contract SignatureCheckerTest is Test {
         bytes32 hash = keccak256("test");
 
         // Replace with our notary signature
+        // 9039BD2DED8FD5E09B62A04A3D25D2D13F0F45F40C1B4CA6C3AECE4A71F92C1416D62561A8939ED49C97D9621C47988CF74B5B5074F89F2B2C6B9B18A6EEB1B2
+        // 9039BD2DED8FD5E09B62A04A3D25D2D13F0F45F40C1B4CA6C3AECE4A71F92C1416D62561A8939ED49C97D9621C47988CF74B5B5074F89F2B2C6B9B18A6EEB1B2
+        // There are interestingly 129 characters in this signature, the ercrecover opcode expects 65 where 32 are R, 32 are S, and 1 is V
         bytes memory signature =
             abi.encodePacked(bytes32(0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef));
 
