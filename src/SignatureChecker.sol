af427675e142f8c8ae15221b1c12c4b93957a4ec
Waylon Jepsen <waylonjepsen1@gmail.com>
2025-02-10 14:36:41 -0700

    passing test

--- src/SignatureChecker.sol
+++ src/SignatureChecker.sol
@@ -2,23 +2,47 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import {ECDSA} from "./ECDSA.sol";
 
 // todo: Do we need to have some stateful capabilities?
-contract SignatureChecker {
-    // todo Replace with our notary address
-    // 0xfdf07A5dCfa7b74f4c28DAb23eaD8B1c43Be801F
-    address public constant NOTARY_ADDRESS = address(0xfdf07A5dCfa7b74f4c28DAb23eaD8B1c43Be801F);
+contract SignatureChecker is Ownable {
+    // Mapping of notary addresses to their validity
+    mapping(address => bool) public isNotary;
 
-    function isValidSignatureNow(bytes32 hash, bytes memory signature) external pure returns (bool) {
-        (address recovered, ECDSA.RecoverError err,) = ECDSA.tryRecover(hash, signature);
-        return err == ECDSA.RecoverError.NoError && recovered == NOTARY_ADDRESS;
+    // error for invalid signatures
+    error InvalidSignature();
+    error InvalidNotary();
 
-        /// this decomposes the signature into r, s, v
+    // constructor configures the notary address
+    constructor(address _notaryAddress) Ownable(msg.sender) {
+        isNotary[_notaryAddress] = true;
+    }
+
+    function addNotary(address _notaryAddress) external onlyOwner {
+        isNotary[_notaryAddress] = true;
+    }
+
+    function removeNotary(address _notaryAddress) external onlyOwner {
+        isNotary[_notaryAddress] = false;
+    }
+
+    function verifySignature(bytes32 _hash, uint8 v, bytes32 r, bytes32 s, address signer)
+        external
+        view
+        returns (bool)
+    {
+        // check if the signer is a notary
+        if (!isNotary[signer]) {
+            revert InvalidNotary();
+        }
+
         // r is the x-coordinate of the curve point
         // s is the y-coordinate of the curve point
-
-        // v is 0 for an even y-value
-        // v is 1 for an odd y-value
+        // v is 27 or 28 based on the y-value being even or odd
+        // verify the signature
+        address recoveredSigner = ecrecover(_hash, v, r, s);
+        if (recoveredSigner != signer) {
+            revert InvalidSignature();
+        }
+        return true;
     }
 }
--- test/SignatureChecker.t.sol
+++ test/SignatureChecker.t.sol
@@ -8,21 +8,29 @@ contract SignatureCheckerTest is Test {
     SignatureChecker public signatureChecker;
 
     function setUp() public {
-        signatureChecker = new SignatureChecker();
+        signatureChecker = new SignatureChecker(0xfdf07A5dCfa7b74f4c28DAb23eaD8B1c43Be801F);
     }
 
     function test_isValidSignatureNow() public view {
-        // Replace with our notary proof hash
-        bytes32 hash = keccak256("test");
-
-        // Replace with our notary signature
-        // 9039BD2DED8FD5E09B62A04A3D25D2D13F0F45F40C1B4CA6C3AECE4A71F92C1416D62561A8939ED49C97D9621C47988CF74B5B5074F89F2B2C6B9B18A6EEB1B2
-        // 9039BD2DED8FD5E09B62A04A3D25D2D13F0F45F40C1B4CA6C3AECE4A71F92C1416D62561A8939ED49C97D9621C47988CF74B5B5074F89F2B2C6B9B18A6EEB1B2
+        
+        // TEST vector from NOTARY
+        // "merkle_root": "0x2943593524b7c19d5448e2489772f3de80d71418c3a62b25ef39559334acbffe", 
+        // "signature" is comprised of 160 characters
+        // 0x3045022100 : Not sure what the firstones here are
+        // 9039bd2ded8fd5e09b62a04a3d25d2d13f0f45f40c1b4ca6c3aece4a71f92c14 : This is the R value
+        // 0220 : Not sure what the secondones here are
+        // 16d62561a8939ed49c97d9621c47988cf74b5b5074f89f2b2c6b9b18a6eeb1b2 : This is the S value
+        // "signature_r": "0x9039bd2ded8fd5e09b62a04a3d25d2d13f0f45f40c1b4ca6c3aece4a71f92c14", 
+        // "signature_s": "0x16d62561a8939ed49c97d9621c47988cf74b5b5074f89f2b2c6b9b18a6eeb1b2", 
+        // "signature_v": 28, 
+        // "signer": "0xfdf07a5dcfa7b74f4c28dab23ead8b1c43be801f"
+        address signer = 0xfdf07A5dCfa7b74f4c28DAb23eaD8B1c43Be801F;
         // There are interestingly 129 characters in this signature, the ercrecover opcode expects 65 where 32 are R, 32 are S, and 1 is V
-        bytes memory signature =
-            abi.encodePacked(bytes32(0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef));
-
+        bytes32 _hash = bytes32(0x2943593524b7c19d5448e2489772f3de80d71418c3a62b25ef39559334acbffe);
+        uint8 v = 28;
+        bytes32 r = bytes32(0x9039bd2ded8fd5e09b62a04a3d25d2d13f0f45f40c1b4ca6c3aece4a71f92c14);
+        bytes32 s = bytes32(0x16d62561a8939ed49c97d9621c47988cf74b5b5074f89f2b2c6b9b18a6eeb1b2);
         // verify signature
-        assertEq(signatureChecker.isValidSignatureNow(hash, signature), true);
+        assertEq(signatureChecker.verifySignature(_hash, v, r, s, signer), true);
     }
 }
